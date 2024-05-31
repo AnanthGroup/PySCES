f4592afed2f7d67233823d0bd72daa17b13c30a4
ChristopherAMyers <myerschris92@gmail.com>
2024-05-31 10:31:17 -0700

    added examples

---
+++ examples/gamess_ethylene/input_simulation_local.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 19:05:28 2023
+
+@author: user
+"""
+
+"""
+Repository of simulation variables
+"""
+
+nel, natom = 3, 6 # number of electronic states, number of atoms in the molecule
+nnuc = 3*natom # number of nuclear DOFs
+ndof = nel + nnuc 
+temp = 300 # simulation temperature in Kelvin
+
+# Initial sampling function ('conventioanl', 'modified', or 'spin' LSC-IVR)
+sampling = 'conventional' 
+
+# Centers of initial electronic coherent states (positions q0 & momenta p0)
+# From left, electronic state 1, 2, 3, ...
+q0 = [0.0, 0.0, 0.0] 
+p0 = [0.0, 0.0, 0.0]
+
+# Center of initial momentum of nuclear modes (same value for all nuc DOFs)
+# NOTE: the centers of initial position are determined by normal coordinates
+pN0 = 0.0
+
+# ELectronic coherent state width parameters
+# From left, electronic state 1, 2, 3, ...
+width = [1.0, 1.0, 1.0]
+
+# Specify an integrator (Choose from 'ABM', 'BSH', and 'RK4')
+integrator = 'RK4'
+# Size of time step (a.u.), number of steps (Only relevant for ABM)
+timestep, nstep = 1.0, 16700
+# Maximum propagation time (a.u.), BSH step to be tried (a.u.), error tolerance (ratio) (Only relevant for BSH)
+tmax_bsh, Hbsh, tol = 10, 3.0, 0.01 
+# Maximum propagation time (a.u.), one Runge-Kutta step (a.u.) (Only relevant for RK4)
+tmax_rk4, Hrk4 = 4200*4, 1.0
+
+# Scaling factor of normal mode frequancies
+frq_scale = 0.967
+
+# Number of CPUs and nodes in each internal NACME calculation
+ncpu, nnode = 1, 1
+
+# Partition of Pool cluster (astra or common)
+partition = 'astra'
+
+# Indices of electronic states for which NACME to be read from GAMESS output file
+elab = [1, 2, 3]
+
+# Index of initially occupied electronic state
+init_state = 2
+
+# Restart request: 0 = no restart, 1 = restart
+restart = 0
+
+#   type of QC runner, either 'gamess or 'terachem'
+QC_RUNNER = 'terachem'
+QC_RUNNER = 'gamess'
+
+#   TeraChem runner options
+tcr_host = '10.1.1.156'
+# tcr_host = 'localhost'
+tcr_port = 9876
+tcr_job_options = {
+        'method': 'camb3lyp',
+        'basis': '6-31g',
+        'charge': 0,
+        'spinmult': 1,
+        'closed_shell': True,
+        'restricted': True,
+        'precision': 'mixed',
+        'convthre': 1E-6,
+}
+tcr_state_options = {
+    'max_state': len(q0)-1, 'grads': 'all', 'NACs': 'all'
+}
+
+# sub_script = '/home/cmyers7/software/gamess_source/rungms'
+sub_script = 'run_gamess.sh'
+
+mol_input_format = 'gamess'
