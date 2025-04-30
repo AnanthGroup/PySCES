ccef0e7f560e9d8391a3c280cece514fd8f82f5c
Ken Miyazaki <66962686+km822@users.noreply.github.com>
2025-04-30 15:00:13 -0500

    Added a function to compute TDM anisotropy

--- pysces/subroutines.py
+++ pysces/subroutines.py
@@ -1848,6 +1848,24 @@ def compute_CF(X, Y):
 
    return()
 
+def compute_anisotropy_correlation(D0, Dt):
+    '''
+    Function to compute transition dipole anisotropy after
+    trajectory propagation.
+    D0 : Transition dipole moment at initial time
+    Dt : Transition dipole moment at time t
+    '''
+    ### Compute the estimator of electronic state population ###
+    aniso_file = 'anisotropy.out'
+
+    with open(os.path.join(__location__, aniso_file), 'a') as f:
+        # Compute the angle between the initial and the present dipole moment
+        cosine = np.dot(D0, Dt)/(np.linalg.norm(D0) * np.linalg.norm(Dt))
+        # The common term for all electronic state projection operators
+        anisotropy = (3.0 * cosine**2 - 1) / 5
+        f.write('{:<12.6f} \n'.format(anisotropy))
+    return()
+    
 '''
 Check which sign for the nac is expected and correct artificial sign flips
 '''
